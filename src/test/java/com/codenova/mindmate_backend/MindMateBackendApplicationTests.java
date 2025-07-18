b1d387d5295c72aff46138568eb8f632995a6d8f
Vihaga Yohan <vihagayohan94@gmail.com>
2025-07-18 15:38:10 +0100

    Initial commit

---
+++ src/test/java/com/codenova/mindmate_backend/MindMateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codenova.mindmate_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindMateBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
